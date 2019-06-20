6f653b98f20b1bb0bb123728710251b1fa871943
Betty <>
2019-06-20 11:55:24 +0200

    basic correl check

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ from experiment import *
 from hasher_implems import *
 
 
-
 def init_experiment(hasher: Hasher):
     exp = Experiment()
     exp.hasher = hasher
     return exp
+
 
 def check_repartition(experiment: Experiment, population: int):
     var1 = 0
@@ -24,9 +24,38 @@ def check_repartition(experiment: Experiment, population: int):
     print(f"Users in variation 2: {var2} or {var2 / population * 100}%")
 
 
-expA = init_experiment(Md5Hasher())
-check_repartition(expA, 10000)
+def check_correlation(experimentA: Experiment, experimentB: Experiment, population: int):
+    usersA1 = []
 
-expB = init_experiment(Sha256Hasher())
-check_repartition(expB, 10000)
+    varB1 = 0
+    varB2 = 0
+
+    for i in range(1, population):
+        user = User(i)
+        var = experimentA.assign(user)
+        if var.name == "variation1":
+            usersA1.append(user)
+
+    for user in usersA1:
+        var = experimentB.assign(user)
+        if var.name == "variation1":
+            varB1 += 1
+        else:
+            varB2 += 1
+
+    print(f"Users in variation B1: {varB1} or {varB1 / len(usersA1) * 100}% of people in var A1, and {varB1 / population * 100}% ot total population")
+    print(f"Users in variation B2: {varB2} or {varB2 / len(usersA1) * 100}% of people in var A1, and {varB2 / population * 100}% ot total population")
+
+
+# MD5
+expA_md5 = init_experiment(Md5Hasher())
+expB_md5 = init_experiment(Md5Hasher())
+check_repartition(expA_md5, 10000)
+check_correlation(expA_md5, expB_md5, 1000000)
+
+# SHA256
+expA_sha256 = init_experiment(Sha256Hasher())
+expB_sha256 = init_experiment(Sha256Hasher())
+check_repartition(expA_sha256, 10000)
+check_correlation(expA_sha256, expB_sha256, 1000000)
 
