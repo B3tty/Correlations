fd4c606e118c3a221ecbdf8766ec78591c766cc2
Betty <>
2019-06-20 11:28:46 +0200

    refacto run

--- run.py
+++ run.py
@@ -3,11 +3,25 @@ from experiment import *
 from md5hasher import Md5Hasher
 
 
-expA = Experiment()
-expA.hasher = Md5Hasher()
+def init_experiment(hasher: Hasher):
+    exp = Experiment()
+    exp.hasher = hasher
+    return exp
 
-for i in range(1, 10):
-    user = User(i)
-    var = expA.assign(user)
-    print(f"user {i} in variation {var.name}")
+def check_repartition(experiment: Experiment, population: int):
+    var1 = 0
+    var2 = 0
 
+    for i in range(1, population):
+        user = User(i)
+        var = experiment.assign(user)
+        if var.name == "variation1":
+            var1 += 1
+        else:
+            var2 += 1
+    print(f"Users in variation 1: {var1} or {var1 / population * 100}%")
+    print(f"Users in variation 2: {var2} or {var2 / population * 100}%")
+
+
+expA = init_experiment(Md5Hasher())
+check_repartition(expA, 10000)
